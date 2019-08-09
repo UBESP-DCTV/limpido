80b318c74a5458f598158d396e0a06c2e8c24b76
Corrado Lanera <corrado.lanera@gmail.com>
2019-08-09 21:21:15 +0200

    cnn exploration

--- analyses/R/020-otiti-cnn.R
+++ analyses/R/020-otiti-cnn.R
@@ -1,33 +1,131 @@
-# Attach packages =================================================
-
+# Packages ========================================================
 library(limpido)
 library(tidyverse)
-library(lubridate)                          # to attach before here
 library(keras)
-library(here)
-library(glue)
-library(usethis)
 
-# Constants =======================================================
 
-data_path <- here("../../data/")
-output_path <- here("../../output/")
-
-random_seed <- sample.int(1e4, 1)
-set.seed(random_seed)
-
-# n_train <- 4600              # cases in the train + train_val set
-validation_len <- 300L                # cases in the validation set
-
-embedding_dim <- 300L                 # embedding vectors dimension
-
-max_words     <- min(                        # max words considered
-    # XXXL  # max number of words the user admit
-    10733L, # all 1gram in the train-validation
-    122607  # all 1-gram in the pretrained
+# Parameters ======================================================
+params <- setup_input_data(
+    validation_len = 300L,
+    max_words = Inf,
+    embedding_dim  = "100",
+    maxlen = 300L,
+    data_path   = here::here("../../data/"),
+    output_path = here::here("../../output/"),
+    random_seed = sample.int(1e4, 1),
+    mixdb_name  = "mixdb_otiti_tagged.rds",
+    verbose = TRUE,
+    batch_size = 8L,
+    epochs = 15L,
+    loss      = "categorical_crossentropy",
+    metrics   = "categorical_accuracy",
+    optimizer = "adam"
 )
-maxlen <- 300L                    # max words/token per observation
 
-batch_size <- 8L                      # sample to pass in each feed
-epochs <- 15L               # whole passage throug the training set
 
+# Model definition ================================================
+architecture <- glue::glue("
+    fixed pedianet embedding +
+    conv2x128_same_relu  + max_pool2 + batch_norm + dropout0.1 +
+    conv2x256_same_relu  + max_pool2 + batch_norm + dropout0.1 +
+    conv3x512_same_relu  + max_pool3 + batch_norm + dropout0.1 +
+    conv5x1024_same_relu + max_pool5 + batch_norm + dropout0.1 +
+    flatten +
+    fc6_softmax
+")
+
+model <- keras_model_sequential() %>%
+    layer_embedding(
+        input_dim = params$max_words,
+        output_dim = params$embedding_dim,
+        input_length = params$maxlen,
+        trainable = FALSE,
+        weights = params$embedding_matrix
+    ) %>%
+    layer_conv_1d(
+        filters = 128,
+        kernel_size = 2L,
+        strides = 1L,
+        padding = "same",
+        activation = "relu"
+    ) %>%
+    layer_max_pooling_1d(2L) %>%
+    layer_batch_normalization() %>%
+    layer_dropout(rate = 0.1) %>%
+    layer_conv_1d(
+        filters = 256,
+        kernel_size = 2L,
+        strides = 1L,
+        padding = "same",
+        activation = "relu"
+    ) %>%
+    layer_max_pooling_1d(2L) %>%
+    layer_batch_normalization() %>%
+    layer_dropout(rate = 0.1) %>%
+    layer_conv_1d(
+        filters = 512,
+        kernel_size = 3L,
+        strides = 1L,
+        padding = "same",
+        activation = "relu"
+    ) %>%
+    layer_max_pooling_1d(3L) %>%
+    layer_batch_normalization() %>%
+    layer_dropout(rate = 0.1) %>%
+    layer_conv_1d(
+        filters = 1024,
+        kernel_size = 5L,
+        strides = 1L,
+        padding = "same",
+        activation = "relu"
+    ) %>%
+    layer_max_pooling_1d(5L) %>%
+    layer_batch_normalization() %>%
+    layer_dropout(rate = 0.1) %>%
+    # layer_global_max_pooling_1d() %>%
+    layer_flatten() %>%
+    # layer_dense(units = 32L, activation = "relu") %>%
+    layer_dense(units = 6L, activation = "softmax")
+
+summary(model)
+
+
+# Model compile ===================================================
+model %>%
+    compile(
+        loss      = params$loss,
+        optimizer = params$optimizer,
+        metrics   = params$metrics
+    )
+
+# Run =============================================================
+{
+start_time <- lubridate::now()
+
+    history <- fit(model,
+        # train ------------------------
+        x = params$train_x,
+        y = params$train_y,
+        # validation -------------------
+        validation_data = list(
+            params$validation_x,
+            params$validation_y
+        ),
+        # learning pace ----------------
+        batch_size = params$batch_size,
+        epochs     = params$epochs
+    )
+
+train_time <- lubridate::now() - start_time
+}
+
+
+
+# Plot and save ===================================================
+# plot(history) not in gg_history b/c keras'plot() is not exported!
+p <- plot(history) %>%
+  gg_history(history, architecture, params, train_time)
+
+save_all_models(
+  model, history, params$output_path, plot = p, start_time
+)
