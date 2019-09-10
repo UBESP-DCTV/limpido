ee95baf56baebadc7a07786c53d38d3a21a32f70
Corrado Lanera <corrado.lanera@gmail.com>
2019-09-10 16:09:08 +0200

    22x done

---
+++ analyses/R/v2xx/210-cnn2_batch16_do07_f256.R
@@ -0,0 +1,202 @@
+# Packages ========================================================
+library(limpido)
+library(tidyverse)
+library(keras)
+library(depigner)
+start_bot_for_chat("otiti")
+errors_to_telegram("otiti")
+
+
+# Parameters ======================================================
+params <- setup_input_data(
+    validation_len = 300L,
+    max_words = Inf,            # ~20% of the full dictionary (Pareto)
+    embedding_dim  = "300",
+    maxlen = 1e3,
+    data_path   = here::here("../../data/"),
+    output_path = here::here("../../output/"),
+    random_seed = 1234L, # sample.int(1e4, 1), #
+    mixdb_name  = "mixdb_otiti_tagged.rds",
+    verbose = TRUE,
+    batch_size = 16L, #8L, #
+    epochs = 300L,
+
+    loss      = "categorical_crossentropy",
+    metrics   = "categorical_accuracy",
+    optimizer = "adam",
+    is_test   = FALSE
+)
+
+
+# Model definition ================================================
+run_name <- "210-cnn2_batch16_do07_f256"
+architecture <- glue::glue("
+    (filename: {run_name})
+
+    fixed pedianet embedding + batch_norm +
+    conv(2x256) + batch_norm + dropout07 +
+    global_max +
+    fc6_softmax
+")
+send_to_telegram(glue::glue("start {run_name}"))
+
+# Layer 1 =========================================================
+
+# l1_input --------------------------------------------------------
+l1_input <- layer_input(shape = c(params$maxlen))
+
+l1_embedding <- l1_input %>%
+  layer_embedding(
+        input_dim = params$max_words,
+        output_dim = params$embedding_dim,
+        input_length = params$maxlen,
+        trainable = FALSE,
+        weights = params$embedding_matrix,
+        name = "l1_embedding"
+    ) %>%
+  layer_batch_normalization(name = "l1_batch-norm") %>%
+  layer_dropout(0.2, name = "l1_dropout")
+
+
+# output ----------------------------------------------------------
+output <- l1_embedding %>%
+    layer_conv_1d(
+        filters = 256L,
+        kernel_size = 2L,
+        padding = "same",
+        activation = "relu",
+        name = "l2_2gram_conv"
+    ) %>%
+    layer_batch_normalization() %>%
+    layer_dropout(0.7) %>%
+    layer_global_max_pooling_1d() %>%
+    layer_dense(units = 6L, activation = "softmax")
+
+
+summary(output)
+
+
+# model -----------------------------------------------------------
+model <- keras_model(l1_input, output)
+
+summary(model)
+
+# Model compile ===================================================
+model %>%
+    compile(
+        loss      = params$loss,
+        optimizer = params$optimizer,
+        metrics   = params$metrics
+    )
+
+
+# dir_log <- file.path("~/2019-otiti/output/logs/", run_name)
+# if (!dir.exists(dir_log)) dir.create(dir_log)
+
+callbacks <- list(
+    callback_early_stopping(
+      "val_categorical_accuracy", # "categorical_accuracy", #
+      patience = 100L,
+      restore_best_weights = TRUE    # set to FALSE during test phase!!!
+    )
+)
+
+
+# Run =============================================================
+{
+# tensorboard(dir_log)
+start_time <- lubridate::now()
+
+    history <- fit(model,
+        # train ------------------------
+        x = params$train_x,
+        y = params$train_y,
+        # validation -------------------
+        validation_data = list(
+            params$validation_x,
+            params$validation_y
+        ),
+        # learning pace ----------------
+        batch_size = params$batch_size,
+        epochs     = params$epochs,
+        # callbacks --------------------
+        callbacks = callbacks
+    )
+
+train_time <- lubridate::now() - start_time
+}
+
+glue::glue("{run_name}: base training finished") %>%
+  send_to_telegram()
+
+# Plot and save ===================================================
+# plot(history) not in gg_history b/c keras'plot() is not exported!
+
+p <- plot(history) %>%
+  gg_history(history, architecture, params, train_time)
+
+send_to_telegram(p)
+
+save_all_models(
+  model, history, params, plot = p, start_time, id = run_name
+)
+
+
+# Fine tuning -----------------------------------------------------
+callbacks <- list(
+    callback_early_stopping(
+      "val_categorical_accuracy", # "categorical_accuracy", # when test #
+      patience = 100L,
+      restore_best_weights = TRUE
+    )
+)
+
+
+model %>%
+  unfreeze_weights()
+
+model %>%
+    compile(
+        loss      = params$loss,
+        optimizer = optimizer_adam(lr = 1e-5), #params$optimizer,
+        metrics   = params$metrics
+    )
+
+{
+start_time <- lubridate::now()
+
+    history <- fit(model,
+        # train ------------------------
+        x = params$train_x,
+        y = params$train_y,
+        # validation -------------------
+        validation_data = list(
+            params$validation_x,
+            params$validation_y
+        ),
+        # learning pace ----------------
+        batch_size = params$batch_size,
+        epochs     = params$epochs,
+        # callbacks --------------------
+        callbacks = callbacks
+    )
+
+train_time <- lubridate::now() - start_time
+}
+
+glue::glue("{run_name}: fine tuning finished") %>%
+  send_to_telegram()
+
+# Plot and save ===================================================
+# plot(history) not in gg_history b/c keras'plot() is not exported!
+p <- plot(history) %>%
+  gg_history(
+    history, architecture, params, train_time, fine_tuned = TRUE
+  )
+
+send_to_telegram(p)
+
+save_all_models(
+  model, history, params, plot = p, start_time, fine_tuned = TRUE,
+  id = run_name
+)
