a4c52dde3533150cda8faa74f1454ba1008ee385
CorradoLanera <corrado.lanera@gmail.com>
2022-02-18 13:52:09 +0100

    remove noisy messages

--- R/expand_punctuations.R
+++ R/expand_punctuations.R
@@ -17,20 +17,14 @@ expand_punctuations <- function(text) {
 expand_punctuations.character <- function(text) {
 
     # the aim is to shrink punctuation repetitions
-
     regex <- "(\\W)\\1*"
-    ui_done("[1/4] regex")
 
     # avoid shrink on elipsis
     text <- stringr::str_replace_all(text, "\\.{3}", " __ELIPSIS__ ")
-    ui_done("[2\4] elipsis")
 
-    # add spaces arrouond (single occurences of) punctuations
+    # add spaces around (single occurrences of) punctuation
     text <- stringr::str_replace_all(text, regex, " \\1 ")
-    ui_done("[3\4] separate punctuation")
 
     # remove extra spaces
-    text <- stringr::str_squish(text)
-    ui_done("[4/4] squish")
-    text
+    stringr::str_squish(text)
 }
